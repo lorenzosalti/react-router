5eb3fd050bd47d07cbb0e748cfc0c35e44f7e86e
lorenzosalti <lorenzosalti@gmail.com>
2025-04-16 17:44:07 +0200

    feat: posts display in PostsList

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 
+const getPostsUrl = "https://jsonplaceholder.typicode.com/posts"
+
+const users = ['Luca', 'Arianna', 'Lucia', 'Marco', 'Giovanni', 'Carlo', 'Camilla', 'Giulia', 'Lorenzo', 'Laura']
 
 function PostsList() {
+  const [postsList, setPostsList] = useState([])
 
+  useEffect(() => {
+    axios.get(getPostsUrl)
+      .then(res => setPostsList(res.data))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
-    <>
+    <main>
       <h2>Posts List</h2>
-    </>
+
+      <ul>
+        {postsList.map(post => <li key={post.id}>
+          <h4>{post.title}</h4>
+          <p>Autore: {users[post.userId - 1]}</p>
+        </li>)}
+      </ul>
+    </main>
   )
 }
 
