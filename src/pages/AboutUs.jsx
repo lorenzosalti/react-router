ad6a7742a76701cc2a354808a2cd53549f342e09
lorenzosalti <lorenzosalti@gmail.com>
2025-04-16 13:05:00 +0200

    feat: created pages .jsx files

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+
+
+
+function AboutUs() {
+
+
+  return (
+    <>
+      <h1>About Us</h1>
+    </>
+  )
+}
+
+export default AboutUs
---
+++ src/pages/PostsList.jsx
@@ -0,0 +1,14 @@
+
+
+
+function PostsList() {
+
+
+  return (
+    <>
+      <h1>Posts List</h1>
+    </>
+  )
+}
+
+export default PostsList
