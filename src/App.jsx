0a6c1643c009abdb175a65d4c88318d2b5868439
lorenzosalti <lorenzosalti@gmail.com>
2025-04-16 13:15:09 +0200

    feat: basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import HomePage from "./pages/HomePage"
+import AboutUs from "./pages/AboutUs"
+import PostsList from "./pages/PostsList"
 
 
 function App() {
 
 
   return (
-    <>
+    <BrowserRouter>
       <h1>React Blog Pages</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/posts" element={<PostsList />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
