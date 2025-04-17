902fdee61ebf6d2351476a214b7bee52f532b930
lorenzosalti <lorenzosalti@gmail.com>
2025-04-17 15:59:39 +0200

    feat: add fetch of a single post based on dinamic parameter

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,9 +1,22 @@
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import axios from "axios"
+
+const getPostsUrl = "https://jsonplaceholder.typicode.com/posts"
 
 
 function PostDetail() {
 
+  const { id } = useParams()
 
-  return <h2>Dettaglio di un Post specifico</h2>
+  useEffect(() => {
+    axios.get(`${getPostsUrl}/${id}`)
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err))
+  }, [])
+
+
+  return <h2>Dettaglio del Post {id}</h2>
 
 
 }
