cd9d33b59b21cb700aba352bb7c246c08da82846
lorenzosalti <lorenzosalti@gmail.com>
2025-04-17 16:07:46 +0200

    feat: dinamic post details rapresentation

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 const getPostsUrl = "https://jsonplaceholder.typicode.com/posts"
@@ -9,14 +9,25 @@ function PostDetail() {
 
   const { id } = useParams()
 
+  const [post, setPost] = useState({})
+
   useEffect(() => {
     axios.get(`${getPostsUrl}/${id}`)
-      .then(res => console.log(res.data))
+      .then(res => {
+        console.log(res.data)
+        setPost(res.data)
+      })
       .catch(err => console.error(err))
   }, [])
 
 
-  return <h2>Dettaglio del Post {id}</h2>
+
+
+  return <div className="container">
+    <h2>{post.title}</h2>
+    <h4>Autore: {post.userId}</h4>
+    <p>{post.body}</p>
+  </div>
 
 
 }
